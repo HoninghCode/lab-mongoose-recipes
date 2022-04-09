4ddac57a72f9f4d69784a8b4d3cf0ba05e27c655
Eric Honingh <erichoningh@codedazur.com>
2022-04-09 12:34:30 +0200

    not done

--- index.js
+++ index.js
@@ -10,14 +10,35 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Asian Glazed Chicken Thighs',
+      level: 'Amateur Chef',
+      ingredients: [
+        '1/2 cup rice vinegar',
+        '5 tablespoons honey',
+        '1/3 cup soy sauce (such as Silver Swan®)',
+        '1/4 cup Asian (toasted) sesame oil',
+        '3 tablespoons Asian chili garlic sauce',
+        '3 tablespoons minced garlic',
+        'salt to taste',
+        '8 skinless, boneless chicken thighs'
+      ],
+      cuisine: 'Asian',
+      dishType: 'main_course',
+      duration: 40,
+      creator: 'Chef LePapu'
+    });
   })
-  .catch(error => {
+  .then((recipe) => {
+    console.log(recipe.title);
+  })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
